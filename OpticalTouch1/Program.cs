d038994b1f6cb8f36e218ab92f8f6ffeac8e4e44
k801030 <b99902079@ntu.edu.tw>
2013-11-29 16:40:49 +0800

    set form as the main file

---
+++ OpticalTouch1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+using USBHIDDRIVER;
+
+
+namespace test2
+{
+    class Program
+    {
+        static USBInterface usb;
+        static string vid = "vid_04d8";
+        static string pid = "pid_003f";
+
+        static void program()
+        {
+            usb = new USBInterface(vid, pid);
+            Console.WriteLine("USB Connection: "+usb.Connect());
+
+
+            usb.startRead();
+            Thread.Sleep(5);
+
+            for (int i = 0; i < 8; i++)
+            {
+                Thread.Sleep(10);
+                Console.WriteLine("usb buffer: " + USBInterface.usbBuffer.Count);
+
+            }
+
+            int count = USBInterface.usbBuffer.Count;
+            for (int i = 0; i < count;i++ )
+            {
+                byte[] buf = (byte[])USBInterface.usbBuffer[i];
+                int bufLength = buf.Length;
+                
+                for (int j = 0; j < bufLength;j++ )
+                    Console.Write(buf[j] + " ");
+                Console.WriteLine();
+            }
+
+            
+            usb.stopRead();
+
+
+            //MyPause();
+        }
+        private static void startRead()
+        {
+            
+            
+            
+            if (USBInterface.usbBuffer ==null)
+            {
+                Console.WriteLine("Get no data from usb.");
+            }
+            /*int count = USBInterface.usbBuffer.Count;
+            foreach(byte buffer in USBInterface.usbBuffer)
+            {
+                
+                Console.WriteLine(USBInterface.usbBuffer);
+            }*/
+   
+        }
+
+        private static void MyPause()
+        {
+            Console.Write("Press any key to continue..");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OpticalTouch1/RealtimeChart.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Forms.DataVisualization.Charting;
+namespace test2
+{
+    class RealtimeChart
+    {
+        private Chart chart = null;
+        private int chartWidth = 720;
+        private int chartHeight = 480;
+        private string nameAxisX = "X";
+        private string nameAxisY = "Y";
+
+        public RealtimeChart()
+        {
+            chart = new Chart();
+
+            ChartArea ctArea = new ChartArea();
+            Legend legend = new Legend();
+            Series series = new Series();  // data and attributes of series
+
+
+        }
+
+        public Chart GetChart
+        {
+            get { return chart; }
+        }
+    }
+}
